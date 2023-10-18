9f7f86b295e064232c88772d8ef9fd5419b0761e
vicsnet <adesanmiadebisi@gmail.com>
2023-10-18 14:25:09 +0100

    Updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,10 +6,10 @@ import AppProvider from '@/app/auth/Context'
 export default function Layout({ children }) {
   return (
     <AppProvider>
-      <main>
+      <main className='bg-white'>
         <section className="h-screen flex">
           <SideBar />
-          <div className="w-[100%] max-h-screen overflow-scrollable px-6">
+          <div className="w-[100%] min-h-screen overflow-y-scroll max-h-screen overflow-scrollable px-6">
             {children}
           </div>
         </section >
