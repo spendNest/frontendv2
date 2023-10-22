d4afe9e4d1e7e4ef1f28174ee43838456be03339
vicsnet <adesanmiadebisi@gmail.com>
2023-10-22 11:40:44 +0100

    updated

--- src/app/auth/contractAddress.js
+++ src/app/auth/contractAddress.js
@@ -1,3 +1,3 @@
-export const factoryAddress ="0x66b5ED505cc1e51821281EB0Fe4db747a5ac97ee";
-// export const factoryAddress ="0x534071b545953c696e061A70B319127216369943";
+// export const factoryAddress ="0x66b5ED505cc1e51821281EB0Fe4db747a5ac97ee";
+export const factoryAddress ="0x7D46Fb379b4A52Fe31628E65753f56769e1baEa5";
 export const tokenAddress ="0xDB3cB4f2688daAB3BFf59C24cC42D4B6285828e9"
